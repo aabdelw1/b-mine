ed808f9149fdfff09b3bbaf6f0a523d8c8f0e568
aabdelw1 <ammar.abdelwahed@mindbodyonline.com>
2025-02-06 23:14:30 -0600

    just kidding

--- src/components/ValentinesApp.tsx
+++ src/components/ValentinesApp.tsx
@@ -87,7 +87,6 @@ const ValentineApp: React.FC = () => {
       <img src="/assets/images/rose-cute.gif" alt="Cute rose" />
 
       <Question>Will you be my valentine?</Question>
-      <Question>By Ammar for Aseel</Question>
       <div>
         <Button
           $isYesButton
